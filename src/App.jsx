388695aba6117b227ed375a384c7e7696a617146
Tiana <tiana.musiyachenko@gmail.com>
2024-04-03 12:41:34 +0500

    shange BrouseRouting on HashRouting

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@
 // import Navbar from './components/Navbar';
 // import About from './components/About';
 import './scss/main.scss';
-import { Route, Routes, BrowserRouter } from 'react-router-dom';
+import { Route, Routes, HashRouter } from 'react-router-dom';
 import Projects from './components/Projects';
 import Portfolio from './components/Portfolio';
 import { useDispatch, useSelector } from 'react-redux';
@@ -18,14 +18,14 @@ function App() {
       {/* <div className="container"> */}
       {/* <div className="box"> */}
       {/* <Portfolio/> */}
-      <BrowserRouter
+      <HashRouter
         basename={process.env.NODE_ENV === 'production' ? '/my_portfolio' : '/'}
       >
         <Routes>
           <Route path="/" element={<Portfolio />} />
           <Route path="/projects/" element={<Projects />} />
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
 
       {/* <Navbar />
           
